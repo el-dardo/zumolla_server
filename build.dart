93d375824a559c20c22cfd3fd15205f0c96cfc13
Ivan Zaera Avellon <izaera@gmail.com>
2013-05-13 12:12:47 +0200

    Clean build.dart file

--- build.dart
+++ build.dart
@@ -2,12 +2,6 @@ import 'dart:io';
 import 'package:stream/rspc.dart' as rspc;
 
 void main() {
-  rspc.build(new Options().arguments);/*, filenameMapper: (filePath) {
-    print("===> $filePath");
-    filePath = filePath.replaceAll("web/client/","web/webapp/");
-    filePath = filePath.replaceAll(".rsp.html",".rsp.dart");
-    return filePath;
-  });
-  */      
+  rspc.build(new Options().arguments);     
 }
 
